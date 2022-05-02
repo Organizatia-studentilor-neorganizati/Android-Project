da22ec5610a63230a7dd43e1f6d16808b0285949
Adrian-Gabriel Vaitis <adriangabriel344@gmail.com>
2022-05-02 15:08:19 +0200

    MainActivity viewmodel

---
+++ app/src/main/java/com/service/gtc/viewmodels/MainActivityViewModel.java
@@ -0,0 +1,28 @@
+package com.service.gtc.viewmodels;
+
+import android.app.Application;
+
+import androidx.annotation.NonNull;
+import androidx.lifecycle.AndroidViewModel;
+import androidx.lifecycle.LiveData;
+
+import com.google.firebase.auth.FirebaseUser;
+import com.service.gtc.data.UserRepository;
+
+public class MainActivityViewModel extends AndroidViewModel {
+
+    private UserRepository userRepository;
+
+    public MainActivityViewModel(@NonNull Application application){
+        super(application);
+        userRepository = UserRepository.getInstance(application);
+    }
+
+    public LiveData<FirebaseUser> getCurrentUser(){
+        return userRepository.getCurrentUser();
+    }
+
+    public void signOut(){
+        userRepository.signOut();
+    }
+}
