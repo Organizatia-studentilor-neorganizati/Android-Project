a5b80bdc095ae4359a994b010654ca4e145fdd20
felixioan <stan.felixioan@gmail.com>
2022-05-02 15:37:54 +0200

    LogInActivity created

---
+++ app/src/main/java/com/service/gtc/userinterface/activities/LogIn.java
@@ -0,0 +1,76 @@
+package com.service.gtc.userinterface.activities;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+import androidx.activity.result.ActivityResultLauncher;
+import androidx.activity.result.contract.ActivityResultContract;
+import androidx.activity.result.contract.ActivityResultContracts;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.lifecycle.ViewModelProvider;
+
+import com.example.android.gtc.R;
+import com.firebase.ui.auth.AuthUI;
+import com.service.gtc.viewmodels.LoginViewModel;
+
+import java.util.Arrays;
+import java.util.List;
+
+public class LogIn extends AppCompatActivity {
+
+    private LoginViewModel loginViewModel;
+
+    ActivityResultLauncher<Intent> activityResultLauncher = registerForActivityResult(
+            new ActivityResultContracts.StartActivityForResult(), result -> {
+                if (result.getResultCode() == RESULT_OK)
+                    switchToMainActivity();
+                else
+                    Toast.makeText(this, "SIGN IN CANCELLED", Toast.LENGTH_SHORT).show();
+            });
+    @Override
+    protected void onCreate(Bundle savedInstanceState)
+    {super.onCreate(savedInstanceState);
+    loginViewModel = new ViewModelProvider(this).get(LoginViewModel.class);
+        loggedInCheck();
+    setContentView(R.layout.activity_login);
+
+    Button loginBtn = findViewById(R.id.loginbtn);
+    loginBtn.setOnClickListener(this::signIn);}
+
+    private void loggedInCheck()
+    {
+        loginViewModel.getCurrentUser().observe(this,user-> {
+            if (user != null)
+                switchToMainActivity();
+
+        });
+
+
+    }
+    private void switchToMainActivity()
+    {
+        startActivity(new Intent(this, MainActivity.class));
+        finish();
+    }
+
+    public void signIn(View v)
+    {    List<AuthUI.IdpConfig> providers = Arrays.asList(
+            new AuthUI.IdpConfig.EmailBuilder().build(),
+            new AuthUI.IdpConfig.GoogleBuilder().build());
+
+        Intent signInIntent = AuthUI.getInstance()
+                .createSignInIntentBuilder()
+                .setAvailableProviders(providers)
+                .build();
+
+        activityResultLauncher.launch(signInIntent);
+    }}
+
+
+
+
+
+
---
+++ app/src/main/java/com/service/gtc/userinterface/activities/MainActivity.java
@@ -0,0 +1,4 @@
+package com.service.gtc.userinterface.activities;
+
+public class MainActivity {
+}
